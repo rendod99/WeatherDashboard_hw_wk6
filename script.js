86bd7ab67e13c7b91ea848c2b6e63e7d8c5cdbed
Daniel Rendon <rendod99@gmail.com>
2020-08-25 21:18:26 -0500

    updated 5 day forcast

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ var city = $("#citySearch").val();
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
             // add temp content to html
-            $("#tempK").text("Temperature (K) " + response.main.temp);
             $("#temp").text("Temperature (F) " + tempF.toFixed(2));
         
             // API call for returning UV Index
@@ -32,7 +31,76 @@ var city = $("#citySearch").val();
             }).then(function(response) {
                 console.log(response);
                 $("#uvIndex").text("UV Index: "+response.value);
-                $("#currentDate").html("<h4>"+response.date_iso +"</h4>")
+                //$("#currentDate").html("<h4>"+response.date_iso +"</h4>")
+
+            });
+
+            var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=3744f2a1eddf8133ab4de7cf5021abda";
+
+            $.ajax({
+                url: queryURL3,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response.list[3].dt_txt);
+                $("#date1").text(response.list[3].dt_txt);
+                
+                console.log(response.list[3].main.temp);
+                $("#temp1").text(response.list[3].main.temp);
+                var tempF = (response.list[3].main.temp - 273.15) * 1.80 + 32;
+                $("#temp1").text("Temp: (F) " + tempF.toFixed(2));
+                
+                console.log(response.list[3].main.humidity);
+                $("#humid1").text(response.list[3].main.humidity+"%");
+
+                
+                console.log(response.list[11].dt_txt);
+                $("#date2").text(response.list[11].dt_txt);
+                
+                console.log(response.list[11].main.temp);
+                $("#temp2").text(response.list[11].main.temp);
+                var tempF = (response.list[11].main.temp - 273.15) * 1.80 + 32;
+                $("#temp2").text("Temp: (F) " + tempF.toFixed(2));
+                
+                console.log(response.list[11].main.humidity);
+                $("#humid2").text(response.list[11].main.humidity+"%");
+
+
+                console.log(response.list[18].dt_txt);
+                $("#date3").text(response.list[18].dt_txt);
+                
+                console.log(response.list[18].main.temp);
+                $("#temp3").text(response.list[18].main.temp);
+                var tempF = (response.list[18].main.temp - 273.15) * 1.80 + 32;
+                $("#temp3").text("Temp: (F) " + tempF.toFixed(2));
+                
+                console.log(response.list[18].main.humidity);
+                $("#humid3").text(response.list[18].main.humidity+"%");
+
+
+                console.log(response.list[26].dt_txt);
+                $("#date4").text(response.list[26].dt_txt);
+                
+                console.log(response.list[26].main.temp);
+                $("#temp4").text(response.list[26].main.temp);
+                var tempF = (response.list[26].main.temp - 273.15) * 1.80 + 32;
+                $("#temp4").text("Temp: (F) " + tempF.toFixed(2));
+                
+                console.log(response.list[26].main.humidity);
+                $("#humid4").text(response.list[26].main.humidity+"%");
+
+
+                console.log(response.list[34].dt_txt);
+                $("#date5").text(response.list[34].dt_txt);
+                
+                console.log(response.list[34].main.temp);
+                $("#temp5").text(response.list[34].main.temp);
+                var tempF = (response.list[34].main.temp - 273.15) * 1.80 + 32;
+                $("#temp5").text("Temp: (F) " + tempF.toFixed(2));
+                
+                console.log(response.list[34].main.humidity);
+                $("#humid5").text(response.list[34].main.humidity+"%");
+
+                
 
             });
       });
