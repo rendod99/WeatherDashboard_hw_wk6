1dc9c662195eb24703a572201f087c5e4b9b2656
Daniel Rendon <rendod99@gmail.com>
2020-08-29 22:08:58 -0500

    updated Readme file

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
-function renderLastCity() {
+
+    //pull city from local storage
+    function renderLastCity() {
     var city = localStorage.getItem("city");
     
     var cityElement = `<li class="list-group-item">${city}</li>`;
-  
-    $(".cities").prepend(cityElement); 
-   
-  }
-
-
-$("#button").on("click", function(event){
+    
+    //prepend city to recentlly searched
+    $(".cities").prepend(cityElement)}
+    var counter=0
+    //counter to limit recentlly searched cities
+    function addCounter(){
+        
+        
+        if (counter < 5){
+            renderLastCity()
+            counter++
+            
+            
+        }else{}
+    };
+    //button to activate API call
+    $("#button").on("click", function(event){
     event.preventDefault();
     var citySearch= $("#citySearch").val();
       localStorage.setItem("city", citySearch);
-      renderLastCity()
-
+      addCounter()
+      apiCall()
+    
       
 
-// API call for returning Current Weather
-var city = $("#citySearch").val();
+        
+            //click event for cities in recent cities list
+            $(".cities").on("click", function(){
+                apiCall()
+            });
+            // API call for returning Current Weather
+            function apiCall(){ 
+            var city = $("#citySearch").val();
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +city+"&appid=3744f2a1eddf8133ab4de7cf5021abda";
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +city+"&appid=3744f2a1eddf8133ab4de7cf5021abda";
     
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+                }).then(function(response) {
             console.log(response);
             $("#cityName").text(response.name);
             $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
             $("#humid").text("Humidity: " + response.main.humidity + "%");
-            //$("#icon1").JSON.parse(response.weather[0].icon);
+            
             var iconCode = response.weather[0].icon;
             var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
-            $("#currentDate").prepend("Today: "+"<img src='" + iconUrl  + "'>");
+            $("#currentDate").html("Today: "+"<img src='" + iconUrl  + "'>");
             
         
             // Convert the temp to fahrenheit
@@ -46,7 +65,7 @@ var city = $("#citySearch").val();
             var lon =(response.coord.lon);
        
             var queryURL2 ="https://api.openweathermap.org/data/2.5/uvi?"+"lat="+lat+"&lon="+lon+"&appid=3744f2a1eddf8133ab4de7cf5021abda";
-
+            //Ajax call to  get UV Index
             $.ajax({
                 url: queryURL2,
                 method: "GET"
@@ -62,16 +81,16 @@ var city = $("#citySearch").val();
                     $("#uvIndex").addClass("uvColorMed")
                 }
                 
-                //$("#currentDate").html("<h4>Today: "+response.weqther.main +"</h4>")
 
             });
-
+            //Ajax call to get 5 day forecast
             var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=3744f2a1eddf8133ab4de7cf5021abda";
 
             $.ajax({
                 url: queryURL3,
                 method: "GET"
             }).then(function(response) {
+                //appending 5 day forecast response to DOM
                 console.log(response);
                 console.log(response.list[3].dt_txt);
                 $("#date1").text(response.list[3].dt_txt);
@@ -104,56 +123,59 @@ var city = $("#citySearch").val();
                 $("#symbol2").html("<img src='https://openweathermap.org/img/w/" + response.list[11].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
 
-                console.log(response.list[18].dt_txt);
-                $("#date3").text(response.list[18].dt_txt);
+                console.log(response.list[19].dt_txt);
+                $("#date3").text(response.list[19].dt_txt);
                 
-                console.log(response.list[18].main.temp);
-                $("#temp3").text(response.list[18].main.temp);
-                var tempF = (response.list[18].main.temp - 273.15) * 1.80 + 32;
+                console.log(response.list[19].main.temp);
+                $("#temp3").text(response.list[19].main.temp);
+                var tempF = (response.list[19].main.temp - 273.15) * 1.80 + 32;
                 $("#temp3").text("Temp: (F) " + tempF.toFixed(2));
                 
-                console.log(response.list[18].main.humidity);
-                $("#humid3").text("Humidity: "+response.list[18].main.humidity+"%");
+                console.log(response.list[19].main.humidity);
+                $("#humid3").text("Humidity: "+response.list[19].main.humidity+"%");
 
-                console.log(response.list[18].weather[0].icon);
-                $("#symbol3").html("<img src='https://openweathermap.org/img/w/" + response.list[18].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+                console.log(response.list[19].weather[0].icon);
+                $("#symbol3").html("<img src='https://openweathermap.org/img/w/" + response.list[19].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
 
-                console.log(response.list[26].dt_txt);
-                $("#date4").text(response.list[26].dt_txt);
+                console.log(response.list[27].dt_txt);
+                $("#date4").text(response.list[27].dt_txt);
                 
-                console.log(response.list[26].main.temp);
-                $("#temp4").text(response.list[26].main.temp);
-                var tempF = (response.list[26].main.temp - 273.15) * 1.80 + 32;
+                console.log(response.list[27].main.temp);
+                $("#temp4").text(response.list[27].main.temp);
+                var tempF = (response.list[27].main.temp - 273.15) * 1.80 + 32;
                 $("#temp4").text("Temp: (F) " + tempF.toFixed(2));
                 
-                console.log(response.list[26].main.humidity);
-                $("#humid4").text("Humidity: "+response.list[26].main.humidity+"%");
+                console.log(response.list[27].main.humidity);
+                $("#humid4").text("Humidity: "+response.list[27].main.humidity+"%");
 
-                console.log(response.list[26].weather[0].icon);
-                $("#symbol4").html("<img src='https://openweathermap.org/img/w/" + response.list[26].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+                console.log(response.list[27].weather[0].icon);
+                $("#symbol4").html("<img src='https://openweathermap.org/img/w/" + response.list[27].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
 
-                console.log(response.list[34].dt_txt);
-                $("#date5").text(response.list[34].dt_txt);
+                console.log(response.list[35].dt_txt);
+                $("#date5").text(response.list[35].dt_txt);
                 
-                console.log(response.list[34].main.temp);
-                $("#temp5").text(response.list[34].main.temp);
-                var tempF = (response.list[34].main.temp - 273.15) * 1.80 + 32;
+                console.log(response.list[35].main.temp);
+                $("#temp5").text(response.list[35].main.temp);
+                var tempF = (response.list[35].main.temp - 273.15) * 1.80 + 32;
                 $("#temp5").text("Temp: (F) " + tempF.toFixed(2));
                 
-                console.log(response.list[34].main.humidity);
-                $("#humid5").text("Humidity: "+response.list[34].main.humidity+"%");
+                console.log(response.list[35].main.humidity);
+                $("#humid5").text("Humidity: "+response.list[35].main.humidity+"%");
 
-                console.log(response.list[34].weather[0].icon);
-                $("#symbol5").html("<img src='https://openweathermap.org/img/w/" + response.list[34].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+                console.log(response.list[35].weather[0].icon);
+                $("#symbol5").html("<img src='https://openweathermap.org/img/w/" + response.list[35].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
                 
 
             });
-      });
-      
+        
+        });
+    
+    
+    };
+    
+});   
 
-    });   
-
- 
+    
